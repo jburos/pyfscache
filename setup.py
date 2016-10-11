98403a406ef04183cb2e3a41ef19c1d9aa245af7
Jacki Buros Novik <jackinovik@gmail.com>
2016-10-11 15:56:30 -0400

    Update setup.py
    resolve StringIO error in python3

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@
 
 import os
 
-from StringIO import StringIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 from ConfigParser import ConfigParser
 from setuptools import setup, find_packages
 
